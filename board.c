c5bf87d6bc6eb5e6d0efcea80f3431cfcfbd7afe
Bichoy Daoud <bichoy.daoud@etu.univ-amu.fr>
2024-05-22 17:43:57 +0200

    fix bug

--- board.c
+++ board.c
@@ -54,22 +54,75 @@ int isValidMove(PawnType board[NUM_CELL][NUM_CELL], int fromRow, int fromCol, in
     if (board[toRow][toCol] != PAWN_NULL) {
         return 0;
     }
-    if (board[fromRow][fromCol] == PAWN_WHITE && toRow == fromRow - 1 && (toCol == fromCol - 1 || toCol == fromCol + 1)) {
-        return 1;
+
+    int rowDiff = abs(fromRow - toRow);
+    int colDiff = abs(fromCol - toCol);
+
+    // Vérifier les mouvements diagonaux pour les pions
+    if (rowDiff == 1 && colDiff == 1) {
+        if (board[fromRow][fromCol] == PAWN_WHITE && toRow < fromRow) {
+            return 1;
+        }
+        if (board[fromRow][fromCol] == PAWN_BLACK && toRow > fromRow) {
+            return 1;
+        }
     }
-    if (board[fromRow][fromCol] == PAWN_BLACK && toRow == fromRow + 1 && (toCol == fromCol - 1 || toCol == fromCol + 1)) {
-        return 1;
+
+    // Vérifier les mouvements diagonaux pour les reines
+    if (board[fromRow][fromCol] == QUEEN_WHITE || board[fromRow][fromCol] == QUEEN_BLACK) {
+        if (rowDiff == colDiff) {
+            int rowDirection = (toRow - fromRow) / rowDiff;
+            int colDirection = (toCol - fromCol) / colDiff;
+            for (int i = 1; i < rowDiff; i++) {
+                if (board[fromRow + i * rowDirection][fromCol + i * colDirection] != PAWN_NULL) {
+                    return 0;
+                }
+            }
+            return 1;
+        }
     }
-    if ((board[fromRow][fromCol] == PAWN_WHITE || board[fromRow][fromCol] == QUEEN_WHITE) && toRow == fromRow - 2 &&
-        (toCol == fromCol - 2 || toCol == fromCol + 2) &&
-        board[fromRow - 1][fromCol - (fromCol - toCol) / 2] == PAWN_BLACK) {
-        return 1;
+
+    // Vérifier les captures pour les pions
+    if (rowDiff == 2 && colDiff == 2) {
+        if ((board[fromRow][fromCol] == PAWN_WHITE && toRow < fromRow) ||
+            (board[fromRow][fromCol] == PAWN_BLACK && toRow > fromRow)) {
+            int middleRow = (fromRow + toRow) / 2;
+            int middleCol = (fromCol + toCol) / 2;
+            if ((board[middleRow][middleCol] == PAWN_WHITE || board[middleRow][middleCol] == QUEEN_WHITE) &&
+                board[fromRow][fromCol] == PAWN_BLACK) {
+                return 1;
+            }
+            if ((board[middleRow][middleCol] == PAWN_BLACK || board[middleRow][middleCol] == QUEEN_BLACK) &&
+                board[fromRow][fromCol] == PAWN_WHITE) {
+                return 1;
+            }
+        }
     }
-    if ((board[fromRow][fromCol] == PAWN_BLACK || board[fromRow][fromCol] == QUEEN_BLACK) && toRow == fromRow + 2 &&
-        (toCol == fromCol - 2 || toCol == fromCol + 2) &&
-        board[fromRow + 1][fromCol - (fromCol - toCol) / 2] == PAWN_WHITE) {
-        return 1;
+
+    // Vérifier les captures pour les reines
+    if (board[fromRow][fromCol] == QUEEN_WHITE || board[fromRow][fromCol] == QUEEN_BLACK) {
+        if (rowDiff == colDiff) {
+            int rowDirection = (toRow - fromRow) / rowDiff;
+            int colDirection = (toCol - fromCol) / colDiff;
+            int foundOpponent = 0;
+            for (int i = 1; i < rowDiff; i++) {
+                int currRow = fromRow + i * rowDirection;
+                int currCol = fromCol + i * colDirection;
+                if (board[currRow][currCol] != PAWN_NULL) {
+                    if (board[currRow][currCol] % 2 != board[fromRow][fromCol] % 2) {
+                        if (foundOpponent) {
+                            return 0;
+                        }
+                        foundOpponent = 1;
+                    } else {
+                        return 0;
+                    }
+                }
+            }
+            return foundOpponent;
+        }
     }
+
     return 0;
 }
 
@@ -77,7 +130,24 @@ void makeMove(PawnType board[NUM_CELL][NUM_CELL], int fromRow, int fromCol, int 
     if (abs(fromRow - toRow) == 2 || abs(fromRow - toRow) > 2) {
         int capturedRow = (fromRow + toRow) / 2;
         int capturedCol = (fromCol + toCol) / 2;
-        board[capturedRow][capturedCol] = PAWN_NULL;
+
+        // Cas où une reine capture sur une distance plus longue
+        if (board[fromRow][fromCol] == QUEEN_WHITE || board[fromRow][fromCol] == QUEEN_BLACK) {
+            int rowDirection = (toRow > fromRow) ? 1 : -1;
+            int colDirection = (toCol > fromCol) ? 1 : -1;
+            capturedRow = fromRow + rowDirection;
+            capturedCol = fromCol + colDirection;
+            while (capturedRow != toRow && capturedCol != toCol) {
+                if (board[capturedRow][capturedCol] != PAWN_NULL && board[capturedRow][capturedCol] % 2 != board[fromRow][fromCol] % 2) {
+                    board[capturedRow][capturedCol] = PAWN_NULL;
+                    break;
+                }
+                capturedRow += rowDirection;
+                capturedCol += colDirection;
+            }
+        } else {
+            board[capturedRow][capturedCol] = PAWN_NULL;
+        }
     }
 
     board[toRow][toCol] = board[fromRow][fromCol];
@@ -104,6 +174,7 @@ int canCapture(PawnType board[NUM_CELL][NUM_CELL], int row, int col) {
 int getCaptureMoves(PawnType board[NUM_CELL][NUM_CELL], int row, int col, Move captureMoves[]) {
     int moveCount = 0;
 
+    // Vérifier les captures pour les pions
     if ((board[row][col] == PAWN_WHITE || board[row][col] == QUEEN_WHITE) && row > 1) {
         if (col > 1 && board[row - 1][col - 1] == PAWN_BLACK && board[row - 2][col - 2] == PAWN_NULL) {
             captureMoves[moveCount++] = (Move){row, col, row - 2, col - 2, row - 1, col - 1, board[row][col]};
@@ -122,26 +193,30 @@ int getCaptureMoves(PawnType board[NUM_CELL][NUM_CELL], int row, int col, Move c
         }
     }
 
+    // Vérifier les captures pour les reines
     if (board[row][col] == QUEEN_WHITE || board[row][col] == QUEEN_BLACK) {
         int directions[4][2] = {{-1, -1}, {-1, 1}, {1, -1}, {1, 1}};
         for (int d = 0; d < 4; d++) {
-            int newRow = row + directions[d][0];
-            int newCol = col + directions[d][1];
+            int rowDir = directions[d][0];
+            int colDir = directions[d][1];
+            int newRow = row + rowDir;
+            int newCol = col + colDir;
             int foundOpponent = 0;
             while (newRow >= 0 && newRow < NUM_CELL && newCol >= 0 && newCol < NUM_CELL) {
-                if (board[newRow][newCol] == PAWN_NULL) {
-                    if (foundOpponent) {
-                        captureMoves[moveCount++] = (Move){row, col, newRow, newCol, newRow - directions[d][0], newCol - directions[d][1], board[row][col]};
+                if (board[newRow][newCol] != PAWN_NULL) {
+                    if (board[newRow][newCol] % 2 != board[row][col] % 2) {
+                        foundOpponent = 1;
+                    } else {
                         break;
                     }
-                } else if (board[newRow][newCol] % 2 != board[row][col] % 2) {
-                    if (foundOpponent) break;
-                    foundOpponent = 1;
                 } else {
-                    break;
+                    if (foundOpponent) {
+                        captureMoves[moveCount++] = (Move){row, col, newRow, newCol, newRow - rowDir, newCol - colDir, board[row][col]};
+                        break;
+                    }
                 }
-                newRow += directions[d][0];
-                newCol += directions[d][1];
+                newRow += rowDir;
+                newCol += colDir;
             }
         }
     }
--- game.c
+++ game.c
@@ -130,8 +130,6 @@ int minmax(PawnType board[NUM_CELL][NUM_CELL], int depth, int isMaximizingPlayer
 }
 
 Move findBestMoveAI(PawnType board[NUM_CELL][NUM_CELL], int player) {
-    Move captureMoves[NUM_CELL * NUM_CELL];
-    int moveCount = 0;
     Move bestMove;
     int bestRating = (player == PAWN_WHITE) ? INT_MIN : INT_MAX;
 
@@ -140,15 +138,15 @@ Move findBestMoveAI(PawnType board[NUM_CELL][NUM_CELL], int player) {
         for (int j = 0; j < NUM_CELL; j++) {
             if ((player == PAWN_WHITE && (board[i][j] == PAWN_WHITE || board[i][j] == QUEEN_WHITE)) ||
                 (player == PAWN_BLACK && (board[i][j] == PAWN_BLACK || board[i][j] == QUEEN_BLACK))) {
-                int captures = getCaptureMoves(board, i, j, captureMoves);
-                for (int k = 0; k < captures; k++) {
+                Move captureMoves[NUM_CELL * NUM_CELL];
+                int captureCount = getCaptureMoves(board, i, j, captureMoves);
+                for (int k = 0; k < captureCount; k++) {
                     PawnType tempBoard[NUM_CELL][NUM_CELL];
                     memcpy(tempBoard, board, sizeof(PawnType) * NUM_CELL * NUM_CELL);
                     makeMove(tempBoard, captureMoves[k].row, captureMoves[k].col, captureMoves[k].toRow, captureMoves[k].toCol);
-                    int rating = evaluateBoard(tempBoard);
+                    int rating = minmax(tempBoard, SEARCH_DEPTH, player == PAWN_WHITE ? 0 : 1);
                     if ((player == PAWN_WHITE && rating > bestRating) ||
                         (player == PAWN_BLACK && rating < bestRating)) {
-                        moveCount = 1;
                         bestRating = rating;
                         bestMove = captureMoves[k];
                     }
@@ -156,29 +154,29 @@ Move findBestMoveAI(PawnType board[NUM_CELL][NUM_CELL], int player) {
             }
         }
     }
+
     // Si aucun mouvement de capture n'est disponible, choisir un mouvement ordinaire
-    if (moveCount == 0) {
-        for (int i = 0; i < NUM_CELL; i++) {
-            for (int j = 0; j < NUM_CELL; j++) {
-                if ((player == PAWN_WHITE && (board[i][j] == PAWN_WHITE || board[i][j] == QUEEN_WHITE)) ||
-                    (player == PAWN_BLACK && (board[i][j] == PAWN_BLACK || board[i][j] == QUEEN_BLACK))) {
-                    Move moves[NUM_CELL * NUM_CELL];
-                    int moveCount = 0;
-                    checkLegalMoves(board, i, j, player, moves, &moveCount);
-                    for (int k = 0; k < moveCount; k++) {
-                        PawnType tempBoard[NUM_CELL][NUM_CELL];
-                        memcpy(tempBoard, board, sizeof(PawnType) * NUM_CELL * NUM_CELL);
-                        makeMove(tempBoard, moves[k].row, moves[k].col, moves[k].toRow, moves[k].toCol);
-                        int rating = evaluateBoard(tempBoard);
-                        if ((player == PAWN_WHITE && rating > bestRating) ||
-                            (player == PAWN_BLACK && rating < bestRating)) {
-                            bestRating = rating;
-                            bestMove = moves[k];
-                        }
+    for (int i = 0; i < NUM_CELL; i++) {
+        for (int j = 0; j < NUM_CELL; j++) {
+            if ((player == PAWN_WHITE && (board[i][j] == PAWN_WHITE || board[i][j] == QUEEN_WHITE)) ||
+                (player == PAWN_BLACK && (board[i][j] == PAWN_BLACK || board[i][j] == QUEEN_BLACK))) {
+                Move moves[NUM_CELL * NUM_CELL];
+                int moveCount = 0;
+                checkLegalMoves(board, i, j, player, moves, &moveCount);
+                for (int k = 0; k < moveCount; k++) {
+                    PawnType tempBoard[NUM_CELL][NUM_CELL];
+                    memcpy(tempBoard, board, sizeof(PawnType) * NUM_CELL * NUM_CELL);
+                    makeMove(tempBoard, moves[k].row, moves[k].col, moves[k].toRow, moves[k].toCol);
+                    int rating = minmax(tempBoard, SEARCH_DEPTH, player == PAWN_WHITE ? 0 : 1);
+                    if ((player == PAWN_WHITE && rating > bestRating) ||
+                        (player == PAWN_BLACK && rating < bestRating)) {
+                        bestRating = rating;
+                        bestMove = moves[k];
                     }
                 }
             }
         }
     }
+
     return bestMove;
 }
